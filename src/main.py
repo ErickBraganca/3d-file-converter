e4087b47e9a7d7950d3d712fdddc5d42859ddd86
ErickBraganca <braganca.designer@gmail.com>
2023-07-09 19:35:49 -0300

    feat: implemented converter function

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import pymeshlab
+
+def file_converter(config):
+
+    # Extract file i/o parameters from view
+    input_file = config["input_path"]
+    output_file = config["output_path"]
+
+    # Create a MeshSet object
+    mesh = pymeshlab.MeshSet()
+
+    # Load the input FBX file
+    mesh.load_new_mesh(input_file)
+
+    # Export the mesh to SAT format
+    mesh.save_current_mesh(output_file)
